1d2ab804e403ca810728fd16fea40925616e67c0
Aravind, Machiry <machiry@cs.ucsb.edu>
2016-07-06 16:18:08 -0700

    Adding scoring

---
+++ cb_score/__init__.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+EXEC_OVER_HEAD_KEY = 'exec'
+MEM_USE_OVER_HEAD = 'mem'
+FILE_SIZE_OVER_HEAD = 'size'
+def compute_over_head(unpatched_perf, patched_perf)
+    """
+
+    :param unpatched_perf:
+    :param patched_perf:
+    :return:
+    """
