ec75e877bd082b68399cf08761341551dbafd99b
Aravind, Machiry <machiry@cs.ucsb.edu>
2016-07-15 14:45:36 -0700

    Renaming pkg structure

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
       name='patch_performance',
       version='0.01',
       packages=['patch_performance'],
-      install_requires=[i.strip() for i in open('requirements.txt').readlines() if 'git' not in i],
       description='A Component that computes scores of all patched binaries and updates DB with corresponding scores.',
       url='https://git.seclab.cs.ucsb.edu/cgc/patch_performance',
 )
